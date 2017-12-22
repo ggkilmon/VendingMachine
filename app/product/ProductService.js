72d99c902c0740f639410946978dba6e4cc9088e
Graham Kilmon <ggkilmon@gmail.com>
2017-12-21 21:50:24 -0500

    add icons

--- app/product/ProductService.js
+++ app/product/ProductService.js
@@ -1,8 +1,8 @@
 angular.module('VendingApp').service('ProductService', function(){
     var products = [];
-    products.push(new Product("Cola", 1.00, 5));
-    products.push(new Product("Chips", .50, 10));
-    products.push(new Product("Candy", .65, 10));
+    products.push(new Product("Cola", 1.00, 5, "si-glyph-tea-cup.svg"));
+    products.push(new Product("Chips", .50, 10, "si-glyph-bag.svg"));
+    products.push(new Product("Candy", .65, 10, "si-glyph-candy.svg"));
 
     this.GetProducts = function(){
         return products;
--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,11 @@
-var Product = function(name, price, inventory){
+var Product = function(name, price, inventory, image){
     var self = this;
 
     self.Name = name;
     self.PriceFormatted = CurrencyFormatted(price);
     self.Price = price;
     self.Inventory = inventory;
+    self.ImageName = image;
 
     self.UpdateInventory = function(quantity){
         self.Inventory += quantity;
